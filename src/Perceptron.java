cb07063fc111d3b5e4a309dfe2ef4beea5598974
lukewalbeck <lukewalbeck@gmail.com>
2018-09-28 18:59:15 -0700

    Added initial Perceptron class with basic fyin function and constructor

---
+++ src/Perceptron.java
@@ -0,0 +1,47 @@
+public class Perceptron {
+    int[] weights;
+    int wb;
+    int alpha;
+    int theta;
+    int yin;
+    int y;
+    int t;
+    int[] xArray;
+
+    //Basic constructor with all modifiable data
+    Perceptron(int[] weights, int wb, int alpha, int theta, int yin, int y, int t, int[] xArray) {
+        this.weights = weights;
+        this.wb = wb;
+        this.alpha = alpha;
+        this.theta = theta;
+        this.yin = yin;
+        this.y = y;
+        this.t = t;
+        this.xArray = xArray;
+    }
+
+
+    /*
+    yin Function to determine the f(yin) that we discussed in class, takes both wb, wi's and xi's
+    to formulate the function:
+    yin = wb + x1w1 + x2w2 + ... + xnwn
+    and then throw that through f(yin) to return 3 possible values
+     */
+    public int yinFunction() {
+        int summation = this.wb;
+        for(int i = 0; i < this.xArray.length; i++) { //this is making the fat assumption that both weights and xArray have the SAME length, probably shouldn't be in end code for this loop
+            summation += (this.weights[i]*this.xArray[i]);
+        }
+        if(summation > 0) {
+            return 1;
+        }
+        else if(summation == 0) {
+            return 0;
+        }
+        else {
+            return -1;
+        }
+    }
+
+
+}
--- src/Project01.java
+++ src/Project01.java
@@ -1,5 +1,23 @@
+
 public class Project01 {
     public static void main(String[] args) {
         System.out.println("hello world");
+
+        //until we can read in data for weights and values, just for testing currently
+        int wb = 0;
+        int[] weights = new int[3];
+        int[] values = new int[2];
+        int alpha = 0;
+        int theta = 0;
+        int yin = 0;
+        int t = 0;
+        int y = 0;
+
+
+        //call perceptron class and run table, displaying it will be called in this class
+        Perceptron test = new Perceptron(weights,wb,alpha,theta,yin,y,t,values);
+        int fyin = test.yinFunction();
+        System.out.println(fyin);
+
     }
 }
