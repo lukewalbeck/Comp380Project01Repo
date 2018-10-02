f6ef5a43e249e57271963b939ef278ac03f5f353
simonfinnie <sfinnie@sandiego.edu>
2018-10-01 19:08:53 -0700

    Started just a few of the outputs.

--- src/Project01.java
+++ src/Project01.java
@@ -1,7 +1,9 @@
+import java.util.Scanner;
+
 
 public class Project01 {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("My name is lubie and I am gay");
 
         //until we can read in data for weights and values, just for testing currently
         int wb = 0;
@@ -12,6 +14,13 @@ public class Project01 {
         int yin = 0;
         int t = 0;
         int y = 0;
+
+        Scanner kb = new Scanner(System.in);
+
+        System.out.println("Welcome to my first neural network - A Perceptron Net!\n");
+        System.out.println("Enter 1 to train using a training file, enter 2 to train using a trained weight settings data file:");
+
+
 
 
         //call perceptron class and run table, displaying it will be called in this class
